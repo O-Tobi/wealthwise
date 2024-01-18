6ab04c9378c555c922cbed4ae7bcbcabe5e8f03c
unknown <dunjoyebiz2020@gmail.com>
2024-01-18 21:03:29 +0100

    registration

--- wealthwise/urls.py
+++ wealthwise/urls.py
@@ -5,5 +5,6 @@ from django.contrib.auth.views import LoginView
 urlpatterns = [
     path('', views.index, name='index'),
     path('login/', LoginView.as_view(template_name='wealthwise/registration/login.html'), name='login'),
+    path('sign_up/', views.sign_up, name='sign_up')
 
 ]
--- wealthwise/views.py
+++ wealthwise/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
+from django.contrib.auth import login, logout, authenticate
 # Create your views here.
 
 def index(request):
     return render(request, 'wealthwise/index.html')
+
+def sign_up(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user=form.save()
+            login (request, user)
+            return redirect ('index') 
+            
+        
+    else:
+        form = RegisterForm()
+
+        return render(request, 'wealthwise/registration/sign_up.html', {
+            "form" : form
+        })
